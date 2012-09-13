75b32b24b052dcc7b9ecaa22897d06573a6cc2fa
Sean Corbett <stcorbett@gmail.com>
2012-09-13 11:02:45 -0500

    test 1

---
+++ inventory/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inventory::Application.config.secret_token = '95a6bf9af0134a59e4b81fca4be5d03bef75d2acec5ae92e117945cbd11671d923fcba25df3a1f674800ba6b15682037d86a00d29ab965af7b04374982c9dd0b'
